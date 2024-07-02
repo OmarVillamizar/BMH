1d86d492bdad027d31e77413e30fdebb1015509f
Omar Villamizar <127413956+OmarVillamizar@users.noreply.github.com>
2024-07-01 22:30:22 -0500

    Update : ComplejidadPDF.js desde branch de Deploy

--- src/components/ComplejidadPDF.js
+++ src/components/ComplejidadPDF.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ComplejidadPDF = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height:'80vh', width: "100%"}}>
-            <div style={{ width: '75%', height: '95%', border: '1px solid #ccc' }}>
-                <iframe title="PDF Viewer" src={`${process.env.PUBLIC_URL}/pdfs/examen.pdf`} width="100%" height="100%" style={{ border: 'none' }}></iframe>
+            <div style={{ width: '68%', height: '95%', border: '1px solid #ccc' }}>
+                <iframe title="PDF Viewer" src={`${process.env.PUBLIC_URL}/pdfs/examen.pdf#zoom=68`} width="100%" height="100%" style={{ border: 'none' }}></iframe>
             </div>
         </div>
     );
